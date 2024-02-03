442e725d7c98f7742d0120ec75be71a335007cb7
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-03 21:39:44 +0000

    Implemented code to send back reply where "type"="generate_ok" and "id"=UNIQUE_ID, and storing latest unique id in backupUniqueId data structure

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-    // "encoding/json"
-    // "log"
-    // maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-	"math/rand"
+	// "encoding/json"
+	// "log"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+var backupUniqueId = -1
+
 func generateUniqueId() int {
-	return rand.Intn(1000000)
+	backupUniqueId = rand.Intn(1000000)
+	return backupUniqueId
 }
 
 
 func main() {
-	for i:=1; i <= 10; i++ {
-		fmt.Println(generateUniqueId())
-	}
+	// for i:=1; i <= 10; i++ {
+	// 	fmt.Println(generateUniqueId())
+	// }
+
+	n := maelstrom.NewNode()
+
+	n.Handle("unique-ids", func(msg maelstrom.Message) error {
+		var requestBody map[string]any
+	
+		if err := json.Unmarshal(msg.Body, &requestBody); err != nil {
+			return err
+		}	
+
+		requestBody["type"] = "generate_ok"
+		requestBody["id"] = generateUniqueId()
+
+		return n.Reply(msg, requestBody)
+	})
 
 }
