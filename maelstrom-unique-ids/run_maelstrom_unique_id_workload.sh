24e84ee752b125057e6475c2b9fda0f1efd3020e
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-04 19:20:09 +0000

    ran test, created scripts to set up env vars, compile program, run test with maelstrom unique id workload, etc.

---
+++ maelstrom-unique-ids/run_maelstrom_unique_id_workload.sh
@@ -0,0 +1,1 @@
+cd maelstrom/ && ./maelstrom test -w unique-ids --bin $GOBIN/maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
---
+++ maelstrom-unique-ids/setup_env_vars.sh
@@ -0,0 +1,3 @@
+export HOME_FOLDER=/workspaces/Distributed-Unique-ID-Generator/
+export GOPATH=/go
+export GOBIN=$GOPATH/bin
